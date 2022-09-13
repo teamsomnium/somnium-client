423c1061e6770af72b7578e14fb434d311fc3276
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-09-14 08:53:07 +0900

    Add Page Template

---
+++ app/components/PageTemplate/PageTemplate.tsx
@@ -0,0 +1,16 @@
+import type { PropsWithChildren } from 'react';
+import { css } from '@emotion/react';
+
+const Template = css`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 48rem;
+  min-height: 100vh;
+  margin: 0 auto;
+  background-color: #ffffff;
+`;
+
+export default function PageTemplate({ children }: PropsWithChildren) {
+  return <div css={Template}>{children}</div>;
+}
