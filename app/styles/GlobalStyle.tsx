aff83c25fae8e0a7b62b9a5eb4fcd8e9bae594a8
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-09-14 20:04:09 +0900

    Add GlobalStyle

--- app/styles/GlobalStyle.tsx
+++ app/styles/GlobalStyle.tsx
@@ -1,4 +1,4 @@
-import { Global, css } from '@emotion/react';
+import { Global, css } from '@emotion/react'
 
 const style = css`
   * {
@@ -6,12 +6,14 @@ const style = css`
     padding: 0;
     font-size: 62.5%;
     box-sizing: border-box;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue',
+      'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
   }
   html {
     background-color: #ededed;
   }
-`;
+`
 
 export default function GlobalStyle() {
-  return <Global styles={style} />;
+  return <Global styles={style} />
 }
