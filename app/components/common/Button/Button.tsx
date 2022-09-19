bce17d51fe2366f022185977941afcc6a81d9b19
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-09-19 23:10:09 +0900

    Update Button Component의 재사용성을 늘림

---
+++ app/components/common/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { css } from '@emotion/react'
+import React from 'react'
+
+interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+  background: string
+  fontColor: string
+}
+
+export default function Button({ background, fontColor, ...rest }: Props) {
+  return <button css={StyledButton(background, fontColor)} {...rest} />
+}
+
+const StyledButton = (background: string, fontColor: string) => css`
+  background-color: ${background};
+  color: ${fontColor};
+  width: 100%;
+  height: 5.3rem;
+  border: none;
+  border-radius: 1.4rem;
+  font-size: 1.6rem;
+  font-weight: 500;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`
