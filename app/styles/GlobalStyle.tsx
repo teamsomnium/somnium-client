990a87fbf31480c90273947b7ab612741e608aec
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-09-19 20:12:11 +0900

    Add Font

--- app/styles/GlobalStyle.tsx
+++ app/styles/GlobalStyle.tsx
@@ -6,8 +6,14 @@ const style = css`
     padding: 0;
     font-size: 62.5%;
     box-sizing: border-box;
+    letter-spacing: -0.1px;
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue',
       'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+    font-weight: 400;
+  }
+  *::selection {
+    background-color: #86a8ff;
+    color: #ffffff;
   }
   html {
     background-color: #ededed;
