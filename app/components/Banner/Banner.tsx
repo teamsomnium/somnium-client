bb0f9d936e432869169439ad3c5d663198d7ee49
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-09-28 10:12:34 +0900

    Banner Style 변경

--- app/components/Banner/Banner.tsx
+++ app/components/Banner/Banner.tsx
@@ -10,32 +10,53 @@ export default function Banner({ name }: Props) {
   const navigate = useNavigate()
   return (
     <section css={Block}>
-      <div css={StyledName}>
-        {name}님, 오늘의 소중한
-        <br /> 꿈을 기록해보아요
+      <div css={StyledBackground} />
+      <div css={LayoutBackground}>
+        <div css={StyledName}>
+          {name}님, 오늘의 소중한
+          <br /> 꿈을 기록해보아요
+        </div>
+        <Button
+          background="#86A8FF"
+          fontColor="#ffffff"
+          onClick={() => {
+            navigate('/write')
+          }}
+        >
+          꿈 기록하기
+        </Button>
       </div>
-      <Button
-        background="#86A8FF"
-        fontColor="#ffffff"
-        onClick={() => {
-          navigate('/write')
-        }}
-      >
-        꿈 기록하기
-      </Button>
     </section>
   )
 }
 
 const Block = css`
+  position: relative;
   height: 23rem;
-  background: linear-gradient(
-    120deg,
-    rgba(255, 206, 230, 1) 0%,
-    rgba(212, 242, 251, 1) 50%,
-    rgba(254, 244, 233, 1) 100%
-  );
-  padding: 6rem 1.8rem 0;
+  margin: 0 -1.8rem;
+  padding: 5.2rem 1.8rem 2.4rem;
+`
+const StyledBackground = css`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    top: 0;
+    background: linear-gradient(
+      120deg,
+      rgba(255, 206, 230, 1) 0%,
+      rgba(212, 242, 251, 1) 50%,
+      rgba(254, 244, 233, 1) 100%
+    );
+  }
 `
 
 const StyledName = css`
@@ -43,5 +64,16 @@ const StyledName = css`
   text-shadow: 0px 0px 13px rgba(0, 0, 0, 50%);
   font-size: 2.4rem;
   font-weight: 600;
-  margin-bottom: 3rem;
+  margin-bottom: 4.5rem;
+  z-index: 2;
+`
+
+const LayoutBackground = css`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 23rem;
+  padding: 5.2rem 1.8rem 2.4rem;
+  z-index: 2;
 `
