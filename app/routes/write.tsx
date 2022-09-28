f4af2d141c3bfb1ad80e69e36c96ea331545500b
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-09-28 10:22:19 +0900

    Write page Outlet 추가

--- app/routes/write.tsx
+++ app/routes/write.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from '@remix-run/react'
 import { Header, HeaderBackButton, PageTemplate } from '~/components'
 import useGoBack from '~/hooks/useGoBack'
 
@@ -6,6 +7,7 @@ export default function Write() {
   return (
     <PageTemplate>
       <Header title="꿈 기록하기" headerLeft={<HeaderBackButton onClick={goBack} />} />
+      <Outlet />
     </PageTemplate>
   )
 }
