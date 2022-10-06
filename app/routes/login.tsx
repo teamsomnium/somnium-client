70aab2f746cad51803edce8404e0b56b7e7eeb65
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-10-06 23:16:22 +0900

    feat: Kakao Login Redirect

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,13 +1,18 @@
 import { Button, PageTemplate } from '~/components'
 import * as I from '~/assets'
 import { css } from '@emotion/react'
+import { CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE } from '~/libs/constants/auth'
 
-export default function login() {
+export default function Login() {
+  function goKakaoLogin() {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+  }
+
   return (
     <PageTemplate>
       <div css={Block}>
         <I.Logo />
-        <Button background="#FAE100" fontColor="#3B1E1E">
+        <Button background="#FAE100" fontColor="#3B1E1E" onClick={goKakaoLogin}>
           <I.Kakao />
           <span css={StyledText}>카카오톡으로 로그인하기</span>
         </Button>
@@ -23,7 +28,7 @@ const Block = css`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding: 10rem 1.8rem;
+  padding: 10rem 0;
 `
 
 const StyledText = css`
