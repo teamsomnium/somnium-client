beb25e3e76fbe82eabd5783760a3c75236eaa943
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2022-10-06 23:17:25 +0900

    feat: handle Textarea Value

--- app/routes/write/intro.tsx
+++ app/routes/write/intro.tsx
@@ -1,8 +1,17 @@
 import { ActiveProgressBar, Button, Label, ProgressBar, Textarea } from '~/components'
 import { css } from '@emotion/react'
+import { useNavigate } from '@remix-run/react'
+import { useState } from 'react'
 
-export default function intro() {
+export default function Intro() {
   const pageNumber = 2
+  const navigate = useNavigate()
+  const [textareaValue, setTextareaValue] = useState<string>('')
+
+  function handleInputValue(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setTextareaValue(e.target.value)
+  }
+
   return (
     <>
       <ProgressBar>
@@ -13,9 +22,15 @@ export default function intro() {
         description={'꿈에 대해 더 자세히 설명해주세요!'}
         username={'임창규'}
       />
-      <Textarea placeholder="ex) 꿈 내용, 꿈을 꾸고 난 후의 기분" />
+      <Textarea placeholder="ex) 꿈 내용, 꿈을 꾸고 난 후의 기분" onChange={handleInputValue} />
       <div css={ButtonWrapper}>
-        <Button background="#86A8FF" fontColor="white">
+        <Button
+          background="#86A8FF"
+          fontColor="white"
+          onClick={() => {
+            navigate('/')
+          }}
+        >
           꿈 기록하기
         </Button>
       </div>
